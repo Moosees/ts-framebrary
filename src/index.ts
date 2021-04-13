95a84365fcbe1790b57b8312a8af941fecbb27f1
Moosees <linus.almgren@gmail.com>
2021-04-13 16:24:19 +0200

    add fetch (by id)  method to User class

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
 import { User } from './models/User';
 
-const user = new User({ age: 123 });
-
-user.on('change', () => {
-  console.log('change');
-});
-user.on('change', () => {
-  console.log('change more');
-});
-user.on('sleep', () => {
-  console.log('sleep');
-});
-
-user.trigger('change');
-user.trigger('sleep');
-user.trigger('nope');
+const user = new User({ id: 1 });
+user.fetch();
