37da40fd82dd6d984222f5d6fd11b6220bb78aaa
Moosees <linus.almgren@gmail.com>
2021-04-21 18:33:14 +0200

    add event handling to UserForm

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,13 +1,34 @@
 export class UserForm {
   constructor(private parent: Element) {}
 
+  onClick(): void {
+    console.log('test');
+  }
+
+  getEvents(): { [key: string]: () => void } {
+    return { 'click:button': this.onClick };
+  }
+
   createTemplate(): string {
     return `<div><input /><button>GO!</button></div>`;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const events = this.getEvents();
+
+    for (let key in events) {
+      const [eventName, selector] = key.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, events[key]);
+      });
+    }
   }
 
   render(): void {
     const template = document.createElement('template');
     template.innerHTML = this.createTemplate();
+    this.bindEvents(template.content);
 
     this.parent.append(template.content);
   }
